da971bd43dbda4cbe71201d81298935034439a01
nankuF <fireloki87@gmail.com>
2022-06-24 15:49:44 +0600

    added code

---
+++ generate_cards.py
@@ -0,0 +1,87 @@
+import os.path
+import random
+
+from faker import Faker
+
+import file_operations
+
+fake = Faker('ru_RU')
+
+
+def get_random_runic_skills() -> []:
+    skills = ['Стремительный прыжок',
+              'Электрический выстрел',
+              'Ледяной удар',
+              'Стремительный удар',
+              'Кислотный взгляд',
+              'Тайный побег',
+              'Ледяной выстрел',
+              'Огненный заряд']
+    selected_skills = random.sample(skills, 3)
+
+    rune_alphabet = {
+        'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠',
+        'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
+        'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠',
+        'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
+        'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
+        'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
+        'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
+        'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
+        'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
+        'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
+        'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
+        'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
+        'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е',
+        'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
+        'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
+        'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
+        'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
+        'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
+        'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
+        'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
+        'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
+        'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
+        ' ': ' '
+    }
+    modified_skill = ''
+    runic_skills = []
+    for skill in selected_skills:
+        for symbol in skill:
+            modified_skill += rune_alphabet[symbol]
+        runic_skills.append(modified_skill)
+        modified_skill = ''
+    return runic_skills
+
+
+def generate_cards(count: int):
+    template_path = os.path.join(os.sep, os.getcwd(), 'src', 'template.svg')
+
+    for i in range(1, count + 1):
+        cards_path = os.path.join(os.sep, os.getcwd(), 'cards', f'result_{i}.svg')
+
+        runic_skills = get_random_runic_skills()
+
+        context = {
+            'first_name': fake.first_name(),
+            'last_name': fake.last_name(),
+            'town': fake.city(),
+            'job': fake.job(),
+            'strength': random.randint(3, 18),
+            'agility': random.randint(3, 18),
+            'endurance': random.randint(3, 18),
+            'intelligence': random.randint(3, 18),
+            'luck': random.randint(3, 18),
+            'skill_1': runic_skills[0],
+            'skill_2': runic_skills[1],
+            'skill_3': runic_skills[2],
+        }
+        file_operations.render_template(template_path, cards_path, context)
+
+
+def main():
+    generate_cards(10)
+
+
+if __name__ == '__main__':
+    main()
